19076243848c8c8357fa1d3e69ba3aa04b37fc37
sociophobic <torikowashi@gmail.com>
2021-03-03 19:03:08 +0300

    serve static

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express()
 
 app.use(cors())
 
-app.use(express.static(path.join(__dirname, 'build')))
+app.use(express.static(path.join(__dirname, 'client/build')))
 
 app.get('/*', (req, res) =>
   res.sendFile(path.join(__dirname, 'client/build', 'index.html')))
