c6d62138ada3065d22b74d61b0f9bc3ac09a038c
sociophobic <torikowashi@gmail.com>
2021-03-03 18:49:55 +0300

    node app

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const path = require('path')
+const cors = require('cors')
+const app = express()
+
+
+app.use(cors())
+
+app.use(express.static(path.join(__dirname, 'build')))
+
+app.get('/*', (req, res) =>
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html')))
+
+
+app.listen(3005)
