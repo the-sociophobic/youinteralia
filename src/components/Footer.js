886c33d7a1233a74ffedf38976715f11ba534d9d
the-sociophobic <torikowashi@gmail.com>
2021-03-23 18:36:56 +0300

    eng logo, style Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,23 +1,28 @@
 import React from 'react'
 
 import ExternalLink from 'components/ExternalLink'
-import { FormattedMessage } from './Store'
+import { StoreContext, FormattedMessage } from './Store'
 import Link from 'components/CustomLink'
 
 
 
-const Footer = () =>
-  <div className="Footer">
-    <ExternalLink to="https://prohelvetia.ru/en/" newTab >
-      <div className="Footer__logo" />
-    </ExternalLink>
-    <Link to='/rules' className='Footer__policy'>
-      <FormattedMessage id='policy' />
-    </Link>
-    <div className='Footer__names'>
-      <FormattedMessage id='names' />
+class Footer extends React.Component {
+  
+  static contextType = StoreContext
+
+  render = () =>
+    <div className="Footer">
+      <ExternalLink to="https://prohelvetia.ru/en/" newTab >
+        <div className={`Footer__logo ${this.context.locale === 'eng' && 'Footer__logo--eng'}`} />
+      </ExternalLink>
+      <Link to='/rules' className='Footer__policy'>
+        <FormattedMessage id='policy' />
+      </Link>
+      <div className='Footer__names'>
+        <FormattedMessage id='names' />
+      </div>
     </div>
-  </div>
+}
 
 
 export default Footer
